f13dfc8ccc3b32a243b46349d49d60493c754700
Biljana Jelaca <bilja.jelaca@gmail.com>
2018-11-23 19:24:57 +0100

    Add Products and Customers components and router-view

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <router-view></router-view> <!--ovo nam smenjuje komponente da ne moramo da ih dodajemo svaku posebnoss-->
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import AppProducts from './components/AppProducts.vue';
+import AppCustomers from './components/AppCustomers.vue';
 
 export default {
   name: 'app',
--- src/main.js
+++ src/main.js
@@ -1,8 +1,33 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-Vue.config.productionTip = false
+import AppCustomers from './components/AppCustomers.vue';
+import AppProducts from './components/AppProducts.vue';
+import VueRouter from 'vue-router'; //importujemo vue-router koji smo instalirali u projektu 'npm install vue-router'
+
+
+
+Vue.config.productionTip = false;
+
+Vue.use(VueRouter); //ovde koristimo VueRouter
+
+
+const routes = [
+  {path: '/', redirect: '/products'},
+  {path: '/products', component: AppProducts}, //putanje - rute
+  {path: '/customers', component: AppCustomers}
+];
+
+
+//instanciranje VueRouter i smestila rute u 'routes'
+const router = new VueRouter({
+  routes
+});
+
+
+
 
 new Vue({
+  router,
   render: h => h(App),
 }).$mount('#app')
